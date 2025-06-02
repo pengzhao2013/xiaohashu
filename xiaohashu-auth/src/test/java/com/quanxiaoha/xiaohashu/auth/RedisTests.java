6f45d1e91b3693cbd64361bd62ceec3b871deadc
pengzhao_2013 <pengzhao_2013@163.com>
2025-06-03 06:50:13 +0800

    feat-story 配置sa-token及redisTemplate

---
+++ xiaohashu-auth/src/test/java/com/quanxiaoha/xiaohashu/auth/RedisTests.java
@@ -0,0 +1,28 @@
+package com.quanxiaoha.xiaohashu.auth;
+
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+
+/**
+ * @author zpstart
+ * @version 1.0
+ * @date 2025-06-02 17:21
+ */
+@SpringBootTest
+@Slf4j
+public class RedisTests {
+    @Resource
+    private RedisTemplate<String, Object> redisTemplate;
+
+    /**
+     * set key value
+     */
+    @Test
+    void testSetKeyValue() {
+        // 添加一个 key 为 name, value 值为 犬小哈
+        redisTemplate.opsForValue().set("name", "犬小哈");
+    }
+}
--- xiaohashu-auth/src/test/java/com/quanxiaoha/xiaohashu/auth/XiaohashuAuthApplicationTests.java
+++ xiaohashu-auth/src/test/java/com/quanxiaoha/xiaohashu/auth/XiaohashuAuthApplicationTests.java
@@ -1,58 +1,65 @@
 package com.quanxiaoha.xiaohashu.auth;
 
+import com.quanxiaoha.framework.common.util.JsonUtils;
+import com.quanxiaoha.xiaohashu.auth.domain.dataobject.UserDO;
+import com.quanxiaoha.xiaohashu.auth.domain.mapper.UserDOMapper;
+import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
 
 @SpringBootTest
 @Slf4j
 class XiaohashuAuthApplicationTests {
 
-//    @Resource
-//    private UserDOMapper userDOMapper;
+    @Resource
+    private UserDOMapper userDOMapper;
 //
 //    /**
 //     * 测试插入数据
 //     */
-//    @Test
-//    void testInsert() {
-//        UserDO userDO = UserDO.builder()
-//                .username("犬小哈test")
-//                .createTime(LocalDateTime.now())
-//                .updateTime(LocalDateTime.now())
-//                .build();
-//
-//        userDOMapper.insert(userDO);
-//    }
-//
-//    @Test
-//    void testSelect() {
-//        // 查询主键 ID 为 4 的记录1
-//        UserDO userDO = userDOMapper.selectByPrimaryKey(2L);
-//        log.info("User: {}", JsonUtils.toJsonString(userDO));
-//    }
-//
-//    /**
-//     * 更新数据
-//     */
-//    @Test
-//    void testUpdate() {
-//        UserDO userDO = UserDO.builder()
-//                .id(2L)
-//                .username("犬小哈教程")
-//                .createTime(LocalDateTime.now())
-//                .updateTime(LocalDateTime.now())
-//                .build();
-//
-//        // 根据主键 ID 更新记录
-//        userDOMapper.updateByPrimaryKey(userDO);
-//    }
+    @Test
+    void testInsert() {
+        UserDO userDO = UserDO.builder()
+                .username("犬小哈test11")
+                .createTime(LocalDateTime.now())
+                .updateTime(LocalDateTime.now())
+                .build();
+
+        userDOMapper.insert(userDO);
+    }
+
+    @Test
+    void testSelect() {
+        // 查询主键 ID 为 4 的记录1
+        UserDO userDO = userDOMapper.selectByPrimaryKey(2L);
+        log.info("User: {}", JsonUtils.toJsonString(userDO));
+    }
+
+    /**
+     * 更新数据
+     */
+    @Test
+    void testUpdate() {
+        UserDO userDO = UserDO.builder()
+                .id(2L)
+                .username("犬小哈教程")
+                .createTime(LocalDateTime.now())
+                .updateTime(LocalDateTime.now())
+                .build();
+
+        // 根据主键 ID 更新记录
+        userDOMapper.updateByPrimaryKey(userDO);
+    }
 
     /**
      * 删除数据
      */
-//    @Test
-//    void testDelete() {
-//        // 删除主键 ID 为 4 的记录
-//        userDOMapper.deleteByPrimaryKey(1L);
-//    }
+    @Test
+    void testDelete() {
+        // 删除主键 ID 为 4 的记录
+        userDOMapper.deleteByPrimaryKey(1L);
+    }
 }
